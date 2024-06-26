034c300e38d78f15360ab907212d3793753106b4
manish-singh_revalize <manish.singh@revalizesoftware.com>
2024-06-27 01:20:23 +0530

    added rating component

--- src/components/details.js
+++ src/components/details.js
@@ -1,17 +1,12 @@
 import React ,{useState, useEffect} from 'react';
 import apicalls from '../apicalls';
+import RateProduct from './rating/rateProduct';
 function Details(props) {
     useEffect(() => {
         const getData = async()=>{
             try {
                 const response = await apicalls.Get("https://fakestoreapi.com/products/" + props?.productId)
                 setproduct(response)
-                // console.log(response);
-                    const rating = [false,false,false,false,false]
-                    for(let i=0;i<Math.round(response.rating.rate);i++){
-                       rating[i] = 1;
-                    }
-                    setrating(rating)
                 // console.log(response);
             } catch (error) {
                 console.log(error);
@@ -60,19 +55,7 @@ function Details(props) {
                         <h4 className='p-2 bg-light m-2 fs-5'>{product.description}</h4>
                         <h4 className='p-2 bg-light m-2 fs-1'> $ {product.price}</h4>
                         <div className='inline ms-3 mb-3'>
-                        {rating.map((each,index)=>
-                        
-                             (<> {rating[index] && <svg xmlns="http://www.w3.org/2000/svg" fill="orange" width={"20px"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="orange" className="size-1 ">
-                             <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                           </svg>
-                           }
-                                    {!rating[index] && <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={"20px"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" className="size-6">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                                        </svg>
-}
-                                    </>
-                        )
-                        )}     
+                        <RateProduct product={product} />  
                         <h6 className='mt-1'>{product.rating.count} ratings</h6>
                         </div>
                         <button className='btn btn-warning m-1 shadow-none' style={{width:"calc(10rem + 10vw)"}} >Buy Now</button>                   
