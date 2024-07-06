60bc61356006842f555931024503ade608fdfd64
manish-singh_revalize <manish.singh@revalizesoftware.com>
2024-07-07 02:03:37 +0530

    changed theme

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,15 +4,15 @@ import './navbar.css'
 function Navbar(props) {
     const navigate = useNavigate();
     return (
-        <div className='bg-maintheme p-3 position-sticky top-0 d-flex justify-content-between align-items-center'>
-            <Link to="/" className='fs-3 fw-bold text-decoration-none text-maintexttheme' title="Home">amazan.in</Link>
-            <div className="w-100 d-flex">
-                <select className="w-25 form-control">
+        <div style={{zIndex:"10"}} className='bg-black p-3 position-sticky top-0 d-flex justify-content-between align-items-center'>
+            <Link to="/" className='fs-3 fw-bold text-decoration-none text-maintheme' title="Home">amazan.in</Link>
+            <div className="w-100 d-flex gap-1 w-md-75 w-50 flex-sm-row flex-row-reverse m-1">
+                {/* <select className="w-25 form-control">
                     <option>All</option>
-                </select>
-                <input placeholder="search Amazon" className="w-100 form-control" />
-                <div className="search-icon">
-                <span className="material-symbols-outlined">search</span>
+                </select> */}
+                <input placeholder="search Amazon" className="w-100 form-control d-none d-sm-block" />
+                <div className="d-flex align-items-center btn btn-outline-maintheme border-0">
+                <span className="material-symbols-outlined float-right">search</span>
                 </div>
             </div>
             <div>
