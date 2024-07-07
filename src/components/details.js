e0d673478288b5b2f52218837a8575c05ac53124
manish-singh_revalize <manish.singh@revalizesoftware.com>
2024-07-07 13:45:49 +0530

    updated details and cart page

--- src/components/details.js
+++ src/components/details.js
@@ -45,21 +45,21 @@ function Details(props) {
       }
     return (
         product &&          
-                    <div className='d-flex flex-md-row flex-column m-3' >
-                        <div className='d-flex flex-column p-3 mx-auto' style={{ width: "calc(3rem + 50%)"}}>
+                    <div className='d-flex flex-md-row flex-column mx-0 mx-sm-2 bg-black' >
+                        <div className='d-flex flex-column p-3 mx-auto bg-white' style={{ width: "calc(3rem + 50%)"}}>
                         <img style={{ width: "calc(3rem + 65%)",maxHeight:"70%"}} className='align-self-center' src={product.image} alt="" />
                         </div>
-                        <div className='d-flex flex-column'>
-                        <h2 className='p-2 bg-light m-2 mb-0'>{product.title}</h2>
+                        <div className='d-flex flex-column bg-maintheme'>
+                        <h2 className='p-2 m-2 mb-0 text-darkyellow'>{product.title}</h2>
                         {/* <h5 className='p-2 bg-light m-2'>{product.category}</h5> */}
-                        <h4 className='p-2 bg-light m-2 fs-5'>{product.description}</h4>
-                        <h4 className='p-2 bg-light m-2 fs-1'> $ {product.price}</h4>
+                        <h4 className='p-2 m-2 fs-5'>{product.description}</h4>
+                        <h4 className='p-2 m-2 fs-1'> $ {product.price}</h4>
                         <div className='inline ms-3 mb-3'>
                         <RateProduct product={product} />  
-                        <h6 className='mt-1'>{product.rating.count} ratings</h6>
+                        <h6 className='mt-1 text-darkyellow'>{product.rating.count} ratings</h6>
                         </div>
-                        <button className='btn btn-warning m-1 shadow-none' style={{width:"calc(10rem + 10vw)"}} >Buy Now</button>                   
-                        <button className='btn btn-secondary m-1 shadow-none' style={{width:"calc(10rem + 10vw)"}} onClick={()=>addToCart(product.id)}>Add To cart</button>                   
+                        <button className='btn btn-dark m-1 shadow-none' style={{width:"calc(10rem + 10vw)"}} >Buy Now</button>                   
+                        <button className='btn btn-outline-dark m-1 shadow-none' style={{width:"calc(10rem + 10vw)"}} onClick={()=>addToCart(product.id)}>Add To cart</button>                   
                         {/* <span className="material-symbols-outlined  fs-5">star_rate star_rate</span> */}
                         {/* <h5 className='p-2 bg-light m-2'> Rated {product.rating.rate}/5 by {product.rating.count} customers</h5> */}
                         </div>
