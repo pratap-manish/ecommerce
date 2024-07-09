8389080fa3da5dde84a419d84c3efa9f8d005c86
manish-singh_revalize <manish.singh@revalizesoftware.com>
2024-07-10 01:55:21 +0530

    added loader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './loader.css'
+import $ from 'jquery';
 function Loader(props) {
+    if(props.isloading){
+        $("body").css("overflow","hidden")
+    }
+    else{
+        $("body").css("overflow","auto")
+    }
     return (
         props.isloading &&
-        <div style={{height:"100%"}} className='bg-secondary position-absolute w-100 opacity-50'>
-        <div className='loader w-100'>
-        <div className="spinner-border" role="status">
+        // <div style={{height:"100%"}} className='bg-secondary position-absolute w-100 opacity-50'>
+        <div className='loader d-flex justify-content-center position-absolute bg-maintexttheme'>
+        <div className="spinner-border text-white position-absolute" style={{width: "3rem", height: "3rem",top:"50%"}} role="status">
   <span className="visually-hidden">Loading...</span>
 </div>
 </div>
-</div>
+// </div>
     );
 }
 
